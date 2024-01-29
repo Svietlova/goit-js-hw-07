142f38f7651725bf9a30fb581b63d6cf11c0275f
Svietlova <irasvet1997@gmail.com>
2024-01-29 02:47:34 +0200

    task-3

--- js/task-3.js
+++ js/task-3.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const input = document.querySelector(`#name-input`);
+const output = document.querySelector(`#name-output`);
+
+input.addEventListener("input", (event) => {
+    const inputValue = event.currentTarget.value.trim();
+
+    if (inputValue !== "") {
+        output.textContent = inputValue;
+    } else {
+        output.textContent = "Anonymous";
+    };
+});
